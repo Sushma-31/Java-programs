d6ae91dcd689f498ce88c230c0bbb12c658b1f79
Sushma Sathishkumar <161341927+Sushma-31@users.noreply.github.com>
2024-02-27 12:26:38 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,20 @@
-import java.util.*;
+
 
 public class Pattern
 {
- 	int i;
-    	Scanner s = new Scanner(System.in);
-	num=s.nextInt();
-	for (int i=1;i<=num;i++)
+public static void main(String [] args)
+{
+ 	int num = 5;
+	for (int i=1;i<=5;i++)
 	{
-		for(int j=1;j<=num;j++)
+		for(int j=1;j<=5;j++)
 		{
-			System.out.println("*");
+			System.out.print("*");
 		}
 	System.out.println();
 
 	}
 	
+}
 }
 
