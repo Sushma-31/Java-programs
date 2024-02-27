b3f077b16aabcaeb9339f7033dcc65a6ae0c32d6
Sushma Sathishkumar <161341927+Sushma-31@users.noreply.github.com>
2024-02-27 12:15:02 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pattern
+{
+ 	int i;
+    	Scanner s = new Scanner(System.in);
+	num=s.nextInt();
+	for (int i=1;i<=num;i++)
+	{
+		for(int j=1;j<=num;j++)
+		{
+			System.out.println("*");
+		}
+	System.out.println();
+
+	}
+	
+}
+
