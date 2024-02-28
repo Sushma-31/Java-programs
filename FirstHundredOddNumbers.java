4420fb137246f7bb1b9104878f8fbe21f84362fd
Sushma Sathishkumar <161341927+Sushma-31@users.noreply.github.com>
2024-02-28 18:51:37 +0530

    Add files via upload

---
+++ FirstHundredOddNumbers.java
@@ -0,0 +1,15 @@
+public class FirstHundredOddNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 1;
+        
+        System.out.println("First 100 odd numbers:");
+        while (count < 100) {
+            if (number % 2 != 0) {
+                System.out.println(number);
+                count++;
+            }
+            number++;
+        }
+    }
+}
