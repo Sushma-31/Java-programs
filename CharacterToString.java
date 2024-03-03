929d148c328d97fa60cf0ea613d3cf4c6bb2eb68
Sushma Sathishkumar <161341927+Sushma-31@users.noreply.github.com>
2024-03-03 21:02:40 +0530

    Add files via upload

---
+++ CharacterToString.java
@@ -0,0 +1,8 @@
+public class CharacterToString {
+    public static void main(String[] args) {
+        char ch = 'A';
+        String strCh = Character.toString(ch);
+         
+        System.out.println("Converted Character to String: " + strCh);
+    }
+}
